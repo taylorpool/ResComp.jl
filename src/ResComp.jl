23a2de1c8410c490a45b7f3c26a883e78c2f94dc
Taylor Pool <taylorpool.27@gmail.com>
2021-09-17 23:06:55 -0600

    Successfully trained reservoir computer on a single signal and can replicate for over 1000 time units

--- src/ResComp.jl
+++ src/ResComp.jl
@@ -1,40 +1,98 @@
 module ResComp
-    using DifferentialEquations;
+using DifferentialEquations
+using LinearAlgebra
+using Plots
 
-    struct AbstractResComp
-    end;
+export UntrainedResComp, TrainedResComp
 
-    export struct UntrainedResComp::AbstractResComp
-        Wᵢₙ
-        u
-        A
-        f
-        γ
-        σ
-        ρ
-    end;
-    
-    export struct TrainedResComp::AbstractResComp
-        Wₒᵤₜ
-        Wᵢₙ
-        u
-        A
-        f
-        γ
-        σ
-        ρ
-    end;
+struct UntrainedResComp
+    Wᵢₙ::AbstractArray
+    u
+    A::AbstractArray
+    f
+    γ::Number
+    σ::Number
+    ρ::Number
+    ϵ::Number
+end;
 
-    function drive!(dr, r, rescomp::UntrainedResComp, t)
-            dr[:] = rescomp.γ.*(-r + rescomp.f.(rescomp.ρ.*rescomp.A*r + rescomp.σ*rescomp.Wᵢₙ*rescomp.u(t)));
+struct TrainedResComp
+    Wₒᵤₜ::AbstractArray
+    Wᵢₙ::AbstractArray
+    u
+    A::AbstractArray
+    f
+    γ::Number
+    σ::Number
+    ρ::Number
+    ϵ::Number
+end;
+
+TrainedResComp(Wₒᵤₜ::AbstractArray, r::UntrainedResComp) = TrainedResComp(
+                                                                          Wₒᵤₜ,
+                                                                          r.Wᵢₙ,
+                                                                          r.u,
+                                                                          r.A,
+                                                                          r.f,
+                                                                          r.γ,
+                                                                          r.σ,
+                                                                          r.ρ,
+                                                                         r.ϵ);
+
+function drive!(dr, r::AbstractArray, rescomp::UntrainedResComp, t)
+        dr[:] = rescomp.γ.*(-r + rescomp.f.(rescomp.ρ.*rescomp.A*r + rescomp.σ*rescomp.Wᵢₙ*rescomp.u(t)));
+end;
+
+function drive!(dr, r::AbstractArray, rescomp::TrainedResComp, t)
+        dr[:] = rescomp.γ.*(-r + rescomp.f.(rescomp.ρ.*rescomp.A*r + rescomp.σ*rescomp.Wᵢₙ*rescomp.Wₒᵤₜ*r));
+end;
+
+function calculateTransientIndex(drive_sol, ϵ)
+    i = 1
+    while norm(drive_sol.u[i]) ≥ ϵ
+            i = i + 1
     end
-    
-    export function train(rescomp::UntrainedResComp, r₀, tspan)
-            drive_prob = ODEProblem(drive!, r₀, tspan, rescomp);
-            drive_sol = solve(drive_prob);
-            D = rescomp.u.(drive_sol.t);
-            R = hcat(sol.u...);
-            Wₒᵤₜ = (R*R' \ R*D)';
-            return Wₒᵤₜ
-    end
+    return i;
+end
+
+function calculateOutputMapping(rescomp::UntrainedResComp, drive_sol)
+        index = calculateTransientIndex(drive_sol, rescomp.ϵ);
+        D = rescomp.u.(drive_sol.t[index:end]);
+        R = hcat(drive_sol.u[index:end]...);
+        Wₒᵤₜ = (R*R' \ R*D)';
+        return Wₒᵤₜ;
+end;
+
+function calculateValidPredictionTime(true_fun, rescomp_pred, ϵ, W)
+        true_prediction = true_fun.(rescomp_pred.t);
+        i = 1;
+        while norm(true_fun(rescomp_pred.t[i]) - W*rescomp_pred.u[i]) < ϵ
+                i += 1;
+        end;
+        return rescomp_pred.t[i] - rescomp_pred.t[1];
+end
+
+function train(rescomp::UntrainedResComp, r₀::AbstractArray, tspan::Tuple{Float64, Float64})
+        drive_prob = ODEProblem(drive!, r₀, tspan, rescomp);
+        drive_sol = solve(drive_prob);
+        Wₒᵤₜ = calculateOutputMapping(rescomp, drive_sol);
+        return TrainedResComp(Wₒᵤₜ, rescomp), drive_sol;
+end;
+
+function test(rescomp::TrainedResComp, r₀::AbstractArray, tspan::Tuple{Float64, Float64})
+        drive_prob = ODEProblem(drive!, r₀, tspan, rescomp);
+        drive_sol = solve(drive_prob);
+        return calculateValidPredictionTime(rescomp.u, drive_sol, rescomp.ϵ, rescomp.Wₒᵤₜ), drive_sol; 
+end;
+
+function plotResults(train_sol, test_sol, rescomp::TrainedResComp)
+    train_nodes = hcat(train_sol.u...)';
+    test_nodes = hcat(test_sol.u...)';
+    test_outputs = test_nodes*rescomp.Wₒᵤₜ';
+    train_node_plot = plot(train_sol, title="Training Nodes")
+    test_node_plot = plot(test_sol, title="Test Nodes")
+    test_output_plot = plot(test_sol.t, test_outputs, title="Test Output")
+    plot(train_node_plot, test_node_plot, test_output_plot, layout=(1,3), legend=false)
+end
+        
 end
