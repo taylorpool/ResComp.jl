8d80a0e6a6614eb01f8b56051dd1460a82ff2ed7
Taylor Pool <taylorpool.27@gmail.com>
2022-01-07 20:48:36 -0700

    Successfully added interaction between python bayesian hyperparameter optimization and julia code base

--- src/Optimize.jl
+++ src/Optimize.jl
@@ -3,7 +3,8 @@ include("ResComp.jl")
 using LinearAlgebra
 using Hyperopt
 using Base.Threads
-
+using PyCall
+import Statistics
 
 function f(system, nₛ, γ, σ, ρ, α)
         nᵣ::Int = 20;
@@ -12,14 +13,14 @@ function f(system, nₛ, γ, σ, ρ, α)
         r₀ = 2*rand(Float64, nᵣ).-0.5
 
         vpts = zeros(10);
-        for i = 1:size(vpts)
+        for i = 1:length(vpts)
 
                 try
                         train_tspan = (0.0, 100.0);
                         test_tspan = (0.0, 100.0) .+ train_tspan[2]
                         trained, train_sol = ResComp.train(untrained, r₀, train_tspan);
                         test_sol = ResComp.test(trained, train_sol.u[end], test_tspan);
-                        vpt = test_sol.t[end] - test_tspan[1];
+                        vpts[i] = test_sol.t[end] - test_tspan[1];
                 catch e
                         if isa(e, LinearAlgebra.SingularException)
                                 @warn "Could not solve least squares formulation"
@@ -27,9 +28,8 @@ function f(system, nₛ, γ, σ, ρ, α)
                                 rethrow()
                         end
                 end;
-
-                return -vpt
         end;
+        return Statistics.mean(vpts), Statistics.std(mean)
 end;
 
 function optimize_rescomp(system,nₛ)
@@ -41,6 +41,49 @@ function optimize_rescomp(system,nₛ)
 
                 @show f(system,nₛ,γ,σ,ρ,α)
         end;
+end;
+
+function evaluate(system, nₛ, parameters)
+        vpt_mean, vpt_std = f(system, nₛ, parameters["gamma"], parameters["sigma"], parameters["rho"], parameters["alpha"]);
+        return vpt_mean, vpt_std;
+end;
+
+function torch_rescomp(system,nₛ)
+        ax_client = PyCall.pyimport("ax.service.ax_client")
+        client = ax_client.AxClient()
+        params = "[
+                {
+                        'name': 'gamma',
+                        'type': 'range',
+                        'bounds': [0.01, 25],
+                        'value_type': 'float'
+                },
+                {
+                        'name': 'sigma',
+                        'type': 'range',
+                        'bounds': [0.01, 5.0],
+                        'value_type': 'float'
+                },
+                {
+                        'name': 'rho',
+                        'type': 'range',
+                        'bounds': [0.01, 25],
+                        'value_type': 'float'
+                },
+                {
+                        'name': 'alpha',
+                        'type': 'range',
+                        'bounds': [0.001, 0.5],
+                        'value_type': 'float'
+                },
+                ]";
+        PyCall.py"$client.create_experiment(name='hello', parameters=$$params, objective_name='vpt', minimize=False)"
+
+        for trial_index = 1:25
+                parameters, trial_index = client.get_next_trial()
+                client.complete_trial(trial_index=trial_index, raw_data=evaluate(system, nₛ, parameters))
+        end
+        return client;
 end;
 
 end;
