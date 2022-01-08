5cf435d12ae096d58ac5f474ec0c198bc9daa7c6
Taylor Pool <taylorpool.27@gmail.com>
2022-01-07 21:17:05 -0700

    Additional refactoring

--- src/Optimize.jl
+++ src/Optimize.jl
@@ -6,46 +6,41 @@ using Base.Threads
 using PyCall
 import Statistics
 
-function f(system, nₛ, γ, σ, ρ, α)
+function find_vpt(untrained, r₀)
+        train_tspan = (0.0, 100.0);
+        test_tspan = (0.0, 100.0) .+ train_tspan[2]
+        trained, train_sol = ResComp.train(untrained, r₀, train_tspan);
+        test_sol = ResComp.test(trained, train_sol.u[end], test_tspan);
+        return test_sol.t[end] - test_tspan[1];
+end
+
+function try_find_vpt(untrained, r₀)
+        try
+                return find_vpt(untrained, r₀);
+        catch e
+                if isa(e, LinearAlgebra.SingularException)
+                        @warn "Could not solve least squares formulation"
+                else
+                        rethrow()
+                end
+        end;
+end
+
+function find_vpts(system, nₛ, γ, σ, ρ, α)
         nᵣ::Int = 20;
-        untrained = ResComp.initialize_rescomp(system, tanh, γ, σ, ρ, nᵣ, nₛ, α)
-            
-        r₀ = 2*rand(Float64, nᵣ).-0.5
 
         vpts = zeros(10);
-        for i = 1:length(vpts)
-
-                try
-                        train_tspan = (0.0, 100.0);
-                        test_tspan = (0.0, 100.0) .+ train_tspan[2]
-                        trained, train_sol = ResComp.train(untrained, r₀, train_tspan);
-                        test_sol = ResComp.test(trained, train_sol.u[end], test_tspan);
-                        vpts[i] = test_sol.t[end] - test_tspan[1];
-                catch e
-                        if isa(e, LinearAlgebra.SingularException)
-                                @warn "Could not solve least squares formulation"
-                        else
-                                rethrow()
-                        end
-                end;
+        @threads for i = 1:length(vpts)
+                untrained = ResComp.initialize_rescomp(system, tanh, γ, σ, ρ, nᵣ, nₛ, α)
+                r₀ = 2*rand(Float64, nᵣ).-0.5
+                vpts[i] = try_find_vpt(untrained, r₀);
         end;
-        return Statistics.mean(vpts), Statistics.std(mean)
-end;
-
-function optimize_rescomp(system,nₛ)
-        ho = @phyperopt for i = 100,
-                γ = LinRange(0.01,25,100),
-                σ = LinRange(0.01, 5.0,100),
-                ρ = LinRange(0.01, 25, 100),
-                α = LinRange(0.001, 0.5, 10)
-
-                @show f(system,nₛ,γ,σ,ρ,α)
-        end;
+        return vpts
 end;
 
 function evaluate(system, nₛ, parameters)
-        vpt_mean, vpt_std = f(system, nₛ, parameters["gamma"], parameters["sigma"], parameters["rho"], parameters["alpha"]);
-        return vpt_mean, vpt_std;
+        vpts = find_vpts(system, nₛ, parameters["gamma"], parameters["sigma"], parameters["rho"], parameters["alpha"]);
+        return Statistics.mean(vpts), Statistics.std(vpts)
 end;
 
 function torch_rescomp(system,nₛ)
