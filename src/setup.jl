7e24816762a6b78916e8c0427a00a1bc4e7e19f2
Taylor Pool <taylorpool.27@gmail.com>
2022-01-10 15:43:14 -0700

    Added more to setup file

--- src/setup.jl
+++ src/setup.jl
@@ -1,3 +1,6 @@
 import Pkg
+Pkg.activate("/fslhome/tpool2/ResComp.jl")
 ENV["PYTHON"] = "/fslhome/tpool2/RCInitialCond/venv/bin/python"
-Pkg.instantiate()
+Pkg.add("PyCall")
+Pkg.add("DifferentialEquations")
+Pkg.add("Erdos")
