9dbe4b02a6835de933b38c95491f51483e683b1b
Taylor Pool <taylorpool.27@gmail.com>
2022-01-18 10:45:46 -0700

    Removed unecessary setup file

--- src/setup.jl
+++
@@ -1,6 +0,0 @@
-import Pkg
-Pkg.activate("/fslhome/tpool2/ResComp.jl")
-ENV["PYTHON"] = "/fslhome/tpool2/RCInitialCond/venv/bin/python"
-Pkg.add("PyCall")
-Pkg.add("DifferentialEquations")
-Pkg.add("Erdos")
